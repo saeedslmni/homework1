1a00ba24383e0c4e6a3f67cde11656125874240b
N Deca <134727270+saeedslmni@users.noreply.github.com>
2023-06-15 05:55:21 +0330

    Update q1.py
    first while completed

--- q1.py
+++ q1.py
@@ -1,12 +1,13 @@
 i=3
-while(i!=2):
+while (i!=2):
     while (i!=2):
+
         print("enter the entering time in hour & minute separated with a space.")
         in1=input()
         in2=list(map(int,in1.split(' ')))
         i=len(in2)
-        if i!=2 or in2[0]>22 or in2[0]<6:
-            print("enter 2 numbers.")
+        if i!=2 or in2[0]>22 or in2[0]<6 or in2[1]>59 or in2[1]<0:
+            print("invalid data.\n Startover")
             i=5
             break
         else:
