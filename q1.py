2f10fb30343e985063b89fb4526fc38a02b68283
N Deca <134727270+saeedslmni@users.noreply.github.com>
2023-06-15 06:18:58 +0330

    Update q1.py

--- q1.py
+++ q1.py
@@ -1,15 +1,25 @@
 i=3
 while (i!=2):
     while (i!=2):
-
         print("enter the entering time in hour & minute separated with a space.")
         in1=input()
         in2=list(map(int,in1.split(' ')))
         i=len(in2)
         if i!=2 or in2[0]>22 or in2[0]<6 or in2[1]>59 or in2[1]<0:
-            print("invalid data.\n Startover")
+            print("invalid data.\nStartover")
             i=5
             break
         else:
-           print("hi")
+            i=3
+            while (i!=2):
+                while (i!=2):
+                    print("enter the leaving time in hour & minute separated with a space.")
+                    in1=input()
+                    in2=list(map(int,in1.split(' ')))
+                    i=len(in2)
+                    if i!=2 or in2[0]>22 or in2[0]<6 or in2[1]>59 or in2[1]<0:
+                        print("invalid data.\nStartover")
+                        i=5
+                        break
+                    break
         break
