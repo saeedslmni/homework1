47fc4d689fe2040b69a99f1d3693171a8d373bb3
N Deca <134727270+saeedslmni@users.noreply.github.com>
2023-06-15 03:37:36 +0330

    Update q1.py

--- q1.py
+++ q1.py
@@ -1,7 +1,13 @@
-inlet1=[0,0];
-print("enter the entering time by hour & minute separated with a space.")
-inlet1=input()
-inlet2=list(map(int,inlet1.split(' ')))
-if len(inlet2)!=2:
-    print("pedarsag!")
-
+i=3;
+a=2;
+while (i!=2):
+    print("enter the entering time by hour & minute separated with a space.")
+    inlet1=input()
+    inlet2=list(map(int,inlet1.split(' ')))
+    i=len(inlet2)
+    if i!=2 or true:
+        print("enter 2 numbers.")
+        break
+    else:
+       print("hi")
+    break
